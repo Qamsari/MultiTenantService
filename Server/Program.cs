9989827991b764eb6e29583877f064e23bf27dfd
Qamsari <44198226+Qamsari@users.noreply.github.com>
2025-04-14 17:10:05 +0330

    edit

--- Server/Program.cs
+++ Server/Program.cs
@@ -9,14 +9,7 @@ builder.Services.AddDbService(builder.Configuration);
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
-//app.Use(async (HttpContext x, RequestDelegate y) => {
-//    var host = x.Request.Host;
-//    if (!x.Response.HasStarted) {
-//        await x.Response.WriteAsync(host.Value??"??");
-//    }
-//});
 app.MapRazorPages();
-//app.MapGet("/", () => "Hello World!");
 using (var scope = app.Services.CreateScope())
 {
     scope.ServiceProvider.GetRequiredService<ISeedData>().SeedData();
